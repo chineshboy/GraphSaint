12b36a7dfaa1321dd4b86b7a198ad59113e4024a
chineshboy <chineshboy@hotmail.com>
2021-07-12 21:16:33 +0800

    bug fix

--- train_large.sh
+++ train_large.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 1 ]; then
   python3 train_sampling.py --gpu $1 --dataset ogbn-papers100M --sampler node --node-budget 6000 --num-repeat 50 --n-epochs 1000 --n-hidden 512 --arch 1-0-1-0
-elif [$# -eq 2 ]; then
+elif [ $# -eq 2 ]; then
   python3 train_sampling.py --gpu $2 --dataset $1 --sampler node --node-budget 6000 --num-repeat 50 --n-epochs 1000 --n-hidden 512 --arch 1-0-1-0
 else
   echo "please specify GPU id and/or dataset"
