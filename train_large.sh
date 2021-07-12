6f9379affddac33dea3cc3160f7505bdf5d4afe5
chineshboy <chineshboy@hotmail.com>
2021-07-12 16:37:22 +0800

    manually select GPU device.

--- train_large.sh
+++ train_large.sh
@@ -1,1 +1,6 @@
-python3 train_sampling.py --gpu 0 --dataset ogbn-papers100M --sampler node --node-budget 6000 --num-repeat 50 --n-epochs 1000 --n-hidden 512 --arch 1-0-1-0
+if [ $# -eq 1 ]; then
+  python3 train_sampling.py --gpu $1 --dataset ogbn-papers100M --sampler node --node-budget 6000 --num-repeat 50 --n-epochs 1000 --n-hidden 512 --arch 1-0-1-0
+else
+  echo "please specify GPU id"
+fi
+
